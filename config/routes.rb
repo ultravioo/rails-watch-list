cad88c84f7407f3861c8843b735a8d02b9a4b20b
Violette Dunoyer de S <violette.dunoyer@gmail.com>
2022-05-19 15:30:27 +0200

    rootttt

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get    'lists',          to: 'lists#index', as: :lists
+  get    'lists/new',      to: 'lists#new',  as: :new_list
+  post   'lists',          to: 'lists#create'
+  get    'lists/:id',      to: 'lists#show', as: :list
+  get    'lists/:list_id/bookmarks/new', to: 'bookmarks#new', as: :new_list_bookmark
+  post 'lists/:list_id/bookmarks', to: 'bookmarks#create', as: :list_bookmarks
+  delete 'bookmarks/:id', to: 'bookmarks#destroy', as: :list_bookmark
 end
